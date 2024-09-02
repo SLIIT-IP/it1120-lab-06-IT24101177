fc6fe31e7a4c4850eb9f6c5ffa5927e3c4242664
IT24101177 <it24101177@my.sliit.lk>
2024-09-02 18:46:20 +0530

    add IT24101177Lab6Q2A.java to Q2A

---
+++ Q2/IT24101177Lab6Q2A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ public class IT24101177Lab6Q2A{
+
+public static void main(String[]args){
+
+ Scanner input=new Scanner(System.in);
+
+int number=1;
+  
+while(number<=10)
+{  System.out.print(number+" ");
+
+   number=number+1; }
+  }
+} 
