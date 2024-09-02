d986fdba036eb3552349f84332bef67b2426ae65
IT24101177 <it24101177@my.sliit.lk>
2024-09-02 18:44:56 +0530

    add IT24101177Lab6Q1.java to Q1

---
+++ Q1/IT24101177Lab6Q1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ public class IT24101177Lab6Q1{
+
+public static void main(String[]args){
+
+ Scanner input=new Scanner(System.in);
+
+double number;
+double square;
+double squareRoot;
+
+System.out.print("Enter a number:");
+number=input.nextDouble();
+
+square=number*number;
+squareRoot=Math.sqrt(number);
+
+System.out.println("The square of "+number+" is:"+square);
+System.out.println("The squareRoot of "+number+" is:"+squareRoot);
+   }
+}
+
+
+
