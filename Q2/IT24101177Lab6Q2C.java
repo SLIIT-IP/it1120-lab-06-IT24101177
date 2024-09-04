a9319a7a8051735b92645992894a8f68418046c5
IT24101177 <it24101177@my.sliit.lk>
2024-09-04 14:26:55 +0530

    add IT24101177Lab6Q2C.java to Q2C

---
+++ Q2/IT24101177Lab6Q2C.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+ public class IT24101177Lab6Q2C{
+
+public static void main(String[]args){
+
+ Scanner input=new Scanner(System.in);
+int num1=0,num2=0,num3=0,num4=0,num5=0,num6=0,num7=0,num8=0,num9=0,num10=0;
+int sum, average;
+System.out.println("Please enter 10 number:");
+ 
+
+        System.out.print("Enter number 01 : ");
+        num1 = input.nextInt();
+        System.out.print("Enter number 02 : ");
+        num2 = input.nextInt();
+        System.out.print("Enter number 03 : ");
+        num3 = input.nextInt();
+        System.out.print("Enter number 04 : ");
+        num4 = input.nextInt();
+        System.out.print("Enter number 05 : ");
+        num5 = input.nextInt();
+        System.out.print("Enter number 06 : ");
+        num6 = input.nextInt();
+        System.out.print("Enter number 07 : ");
+        num7 = input.nextInt();
+        System.out.print("Enter number 08 : ");
+        num8 = input.nextInt();
+        System.out.print("Enter number 09 : ");
+        num9 = input.nextInt();
+        System.out.print("Enter number 10 : ");
+        num10 = input.nextInt();
+
+
+ System.out.println("The number you Entered are:");
+ System.out.println(" " + num1+" "+num2+" "+ num3+" "+num4+" "+ num5+" "+num6+" "+ num7+" "+num8+" "+num9+" "+num10);
+  
+sum=num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+average=sum/10;
+
+   System.out.println("The sum of numbers:"+sum);
+   System.out.println("The average of numbers:"+average);
+  }
+} 
